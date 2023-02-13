9cd67f7664123d11308704eaca5c35e13050d2e1
kienhee <kienhee.it@gmail.com>
2023-02-13 19:18:39 +0700

    upload api

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 /** @format */
 
-export const baseURL = "http://localhost:8088/api";
+export const baseURL = "https://kienhee-portfilio-server.onrender.com/api";
 export const AUTH_LOGIN = "/auth/login";
 export const CREATE_PROJECT = "/project/create-project";
